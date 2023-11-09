34e35be427c652e618e60f4189fdd100c9250934
Rewinell <rewiinell.guardiano@students.ittralee.ie>
2023-11-09 07:56:44 +0000

    tried doing testing

---
+++ VendingMachineSYSTests/CategoriesTests.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VendingMachineSYS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+
+[TestClass]
+public class CategoriesTests
+{
+    [TestMethod]
+    public void TestSetCategory()
+    {
+        // Arrange
+        Categories category = new Categories();
+
+        // Provide test data
+        int catID = 1;
+        string name = "Burgers";
+        string description = "Beef and Chicken";
+
+        category.SetCategory(catID, name, description);
+
+
+        // Verify that the category has been inserted into the database
+        /*
+        // Create a connection to your Oracle database using the same connection string (DBConnect.oradb)
+        using (OracleConnection conn = new OracleConnection(DBConnect.oradb))
+        {
+            conn.Open();
+
+            // Execute a SQL query to retrieve the category you just inserted
+            string sqlQuery = "SELECT * FROM CATEGORIES WHERE CatID = :catID";
+            using (OracleCommand cmd = new OracleCommand(sqlQuery, conn))
+            {
+                cmd.Parameters.Add(new OracleParameter(":catID", catID));
+
+                using (OracleDataReader dr = cmd.ExecuteReader())
+                {
+                    // Check if there is a result
+                    if (dr.Read())
+                    {
+                        // Verify the properties of the retrieved category
+                        Assert.AreEqual(catID, dr.GetInt32(dr.GetOrdinal("CatID")));
+                        Assert.AreEqual(name, dr.GetString(dr.GetOrdinal("Name")));
+                        Assert.AreEqual(description, dr.GetString(dr.GetOrdinal("Description")));
+                    }
+                    else
+                    {
+                        Assert.Fail("Category not found in the database.");
+                    }
+        */
+                }
+            }
+       
---
+++ VendingMachineSYSTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("VendingMachineSYSTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VendingMachineSYSTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("16bff37c-7608-4434-b251-8a1576f5efca")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
